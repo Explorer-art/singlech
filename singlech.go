11331607e81372a3fedbd86be933e5c669bf1389
Truzme_ <56657421+Explorer-art@users.noreply.github.com>
2025-09-15 23:45:05 +0300

    feat: file handlers

--- singlech.go
+++ singlech.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"bufio"
+	"strings"
+	"path/filepath"
 )
 
 var sources []string
 var output string
+var implIncludes []string
+var headerIncludes []string
+
+func containsSlice(array []string, value string) bool {
+	for _, v := range array {
+		if v == value {
+			return true
+		}
+	}
+
+	return false
+}
 
 func parseArgs() {
 	parseMode := 0 // 0 - sources file; 1 - output files name
@@ -26,6 +42,64 @@ func parseArgs() {
 	}
 }
 
+func ImplHandler(path string) string {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(path + " not exists")
+	}
+
+	defer file.Close()
+
+	var code string
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		lineSplited := strings.Split(line, " ")
+
+		if len(lineSplited) > 1 && lineSplited[0] == "#include" {
+			if !containsSlice(implIncludes, line) {
+				implIncludes = append(implIncludes, line)
+			}
+		} else if len(lineSplited) > 1 && lineSplited[0] == "#pragma" && lineSplited[1] == "once" {
+			continue
+		} else {
+			code += line + "\n"
+		}
+	}
+
+	return code
+}
+
+func HeaderHandler(path string) string {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(path + " not exists")
+	}
+
+	defer file.Close()
+
+	var code string
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		lineSplited := strings.Split(line, " ")
+
+		if len(lineSplited) > 1 && lineSplited[0] == "#include" {
+			if !containsSlice(headerIncludes, line) {
+				headerIncludes = append(headerIncludes, line)
+			}
+		} else if len(lineSplited) > 1 && lineSplited[0] == "#pragma" && lineSplited[1] == "once" {
+			continue
+		} else {
+			code += line + "\n"
+		}
+	}
+
+	return code
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Printf("Usage: %s <sources> [flags]\n", os.Args[0])
@@ -41,6 +115,18 @@ func main() {
 
 	parseArgs()
 
-	fmt.Println(sources)
-	fmt.Println(output)
+	// fmt.Println(sources)
+	// fmt.Println(output)
+
+	for _, file := range sources {
+		fileExt := filepath.Ext(file)
+
+		if fileExt == ".c" {
+			fmt.Println(ImplHandler(file))
+		} else if fileExt == ".h" {
+			fmt.Println(HeaderHandler(file))
+		}
+	}
+
+	// fmt.Println(includes)
 }
